38da03c86b0083f5cfc76a98473700372eabde56
AntsmithCode <hey@antsmith.dev>
2021-02-04 20:00:17 -0500

    (App) Changed to ES6 import

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
-const express = require("express");
-const app = express();
+import Express from "express";
+const app = Express();
 const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/balance", (req, res) => {
-  res.send("400");
+app.post("/balance", function (req, res) {
+  // do something with data
+  // req.body.params is the standard for that
+  // for example: req.body.balance
 });
 
 app.listen(port, () => {
-  console.log("Bitcoin Server");
+  console.log("Bitcoin Server Is working");
 });
